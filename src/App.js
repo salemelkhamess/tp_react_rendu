4ff61f751f3bcd5aa1e7e66085e64568871e8e23
salemelkhamess <salemelkhamess@gmail.com>
2025-01-27 20:19:01 +0000

    Créeation du  LanguageContext , Ajoute du  sélecteur de langue

--- src/App.js
+++ src/App.js
@@ -2,33 +2,34 @@ import React, { createContext, useState } from 'react';
 import ProductList from './components/ProductList';
 import ProductSearch from './components/ProductSearch';
 import ThemeToggle from './components/ThemeToggle';
-
-// TODO: Exercice 2.1 - Créer le LanguageContext
+import LanguageSelector from './components/LanguageSelector';
+import { LanguageProvider } from './context/LanguageContext'; // Importer le LanguageProvider
 
 export const ThemeContext = createContext();
 
 const App = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
-  // TODO: Exercice 2.2 - Ajouter l'état pour la langue
 
   return (
-    <ThemeContext.Provider value={{ isDarkTheme, setIsDarkTheme }}>
-      {/* TODO: Exercice 2.1 - Wrapper avec LanguageContext.Provider */}
-      <div className={`container ${isDarkTheme ? 'bg-dark text-light' : 'bg-light'}`}>
-        <header className="my-4">
-          <h1 className="text-center">Catalogue de Produits</h1>
-          <div className="d-flex justify-content-end gap-2">
-            <ThemeToggle />
-            {/* TODO: Exercice 2.2 - Ajouter le sélecteur de langue */}
+      <ThemeContext.Provider value={{ isDarkTheme, setIsDarkTheme }}>
+        {/* Wrapper avec LanguageContext.Provider */}
+        <LanguageProvider>
+          <div className={`container ${isDarkTheme ? 'bg-dark text-light' : 'bg-light'}`}>
+            <header className="my-4">
+              <h1 className="text-center">Catalogue de Produits</h1>
+              <div className="d-flex justify-content-end gap-2">
+                <ThemeToggle />
+                {/* Ajouter le sélecteur de langue */}
+                <LanguageSelector />
+              </div>
+            </header>
+            <main>
+              <ProductList />
+            </main>
           </div>
-        </header>
-        <main>
-          <ProductSearch />
-          <ProductList />
-        </main>
-      </div>
-    </ThemeContext.Provider>
+        </LanguageProvider>
+      </ThemeContext.Provider>
   );
 };
 
-export default App
+export default App;
---
+++ src/context/LanguageContext.js
@@ -0,0 +1,18 @@
+import {useState ,useEffect} from  'react'
+
+const useDebounce = (value, delay) => {
+    const [debouncedValue,setDebouncedValue] = useState(value);
+    useEffect(() =>{
+        const handler = setTimeout(() =>{
+            setDebouncedValue(value);
+        } ,delay);
+
+        return() =>{
+            clearInterval(handler)
+        }
+    },[value,delay]);
+
+    return debouncedValue;
+}
+
+export default useDebounce;
