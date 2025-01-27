a192bf2403c6419c0b592b2c4b96ea67c76d600b
salemelkhamess <salemelkhamess@gmail.com>
2025-01-27 20:05:40 +0000

    Modification de la composant ProductSearch pour utiliser la recherche , Implémentation due debounce sur la recherche

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,24 +1,32 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { ThemeContext } from '../App';
+import useDebounce from '../hooks/useDebounce';
+import { useLanguage } from '../context/LanguageContext'; // Importer useLanguage
 
-const ProductSearch = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const { isDarkTheme } = useContext(ThemeContext);
-  // TODO: Exercice 2.1 - Utiliser le LanguageContext
-  
-  // TODO: Exercice 1.2 - Utiliser le hook useDebounce
-  
-  return (
-    <div className="mb-4">
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Rechercher un produit..."
-        className={`form-control ${isDarkTheme ? 'bg-dark text-light' : ''}`}
-      />
-    </div>
-  );
+
+const ProductSearch = ({ onSearch = () => {} }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const { isDarkTheme } = useContext(ThemeContext);
+    const { translate } = useLanguage(); // Utiliser le contexte de langue
+
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    useEffect(() => {
+        onSearch(debouncedSearchTerm);
+
+    }, [debouncedSearchTerm, onSearch]);
+
+    return (
+        <div className="mb-4">
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder={translate('searchPlaceholder')} // Utiliser la traduction
+                className={`form-control ${isDarkTheme ? 'bg-dark text-light' : ''}`}
+            />
+        </div>
+    );
 };
 
 export default ProductSearch;
